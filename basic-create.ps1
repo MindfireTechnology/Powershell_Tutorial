5d642ea7b4a2936f6d656743da2bde893d0cdace
Daniel Beus <dbeus@mindfiretechnology.com>
2020-01-07 18:00:12 -0700

    Updated to have variables, multiple commands, and uses piping

--- basic-create.ps1
+++ basic-create.ps1
@@ -1,1 +1,11 @@
-﻿New-Item README.md
+﻿$fileName = "README.md"
+$filePath = "."
+$path = "$filePath\$fileName"
+
+New-Item $path -Force
+
+$contents = "# Basic Script$([System.Environment]::NewLine)Script that creates this README, outputs this content, and then shows the content on the screen"
+
+$contents | Out-File $path -Append
+
+Get-Content $path
