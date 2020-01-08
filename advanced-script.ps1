3abc4d29e7a342ad590a4e79501d1b9f2f559515
Daniel Beus <dbeus@mindfiretechnology.com>
2020-01-07 19:17:51 -0700

    Added advanced script with cmdlet binding

---
+++ advanced-script.ps1
@@ -0,0 +1,34 @@
+﻿function Filter-WeekdayItems {
+    [CmdletBinding(SupportsShouldProcess=$True)]
+    param (
+        [Parameter(ValueFromPipeline=$True)]
+        [string[]]$Files,
+
+        [Parameter()]
+        [switch]$Force
+    )
+
+    Begin {
+        Write-Host "Filter-WeekdayItems is starting"
+    }
+
+    Process {
+        Write-Debug "Processing files: $Files"
+        
+        foreach ($file in $Files) {
+            $details = Get-Item $file
+            if ($details.LastWriteTime.DayOfWeek -gt 0 -and $details.LastWriteTime.DayOfWeek -lt 6) {
+                # Confirm with the user that we want to proceed
+                if ($Force -or $PSCmdlet.ShouldProcess($file, "Remove file?")) {
+                    Remove-Item $file
+                }
+            }
+        }
+    }
+
+    End {
+        Write-Host "Filter-WeekdayItems is ending"
+    }
+}
+
+#Filter-WeekdayItems -Files "
